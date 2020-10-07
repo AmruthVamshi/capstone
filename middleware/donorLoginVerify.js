e88d8619818d9dd6aefdb5e5861721b13369856f
AmruthVamshi <battaamruth1@gmail.com>
2020-10-07 17:31:40 +0530

    minor changes

--- middleware/donorLoginVerify.js
+++ middleware/donorLoginVerify.js
@@ -3,8 +3,8 @@ const {Donor} = require('../models');
 module.exports = async (req,res,next)=>{
 	//only for debugging
 	//TODO:remove this
-	const donor = await Donor.findByPk(req.query.id);
-	req.user=donor;
+	//const donor = await Donor.findByPk(req.query.id);
+	//req.user=donor;
 	if(req.user){
 		if(req.user.donorID) return next();
 		else res.sendStatus(401);
