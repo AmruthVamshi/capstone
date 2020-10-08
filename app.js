55766e081509560e29af601ba6ff1624a6bd2159
AmruthVamshi <battaamruth1@gmail.com>
2020-10-08 14:21:23 +0530

    added frontend

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ const app = express();
 })();
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 require('./config/passport.setup');
